5fda4128247b894baf08f353e1469c1d2e6ab251
Matt Stevens <matt@mattstevens.co.uk>
2021-10-27 23:35:19 +0100

    Replace obsolete camera properties

--- code/Camera/FreeCamera.cs
+++ code/Camera/FreeCamera.cs
@@ -19,9 +19,9 @@ namespace Minigolf
 			TargetPos = CurrentView.Position;
 			TargetRot = CurrentView.Rotation;
 
-			Pos = TargetPos;
-			Rot = TargetRot;
-			LookAngles = Rot.Angles();
+			Position = TargetPos;
+			Rotation = TargetRot;
+			LookAngles = Rotation.Angles();
 		}
 
 		public override void Update()
@@ -29,7 +29,7 @@ namespace Minigolf
 			var player = Local.Client;
 			if ( player == null ) return;
 
-			var tr = Trace.Ray( Pos, Pos + Rot.Forward * 4096 ).UseHitboxes().Run();
+			var tr = Trace.Ray( Position, Position + Rotation.Forward * 4096 ).UseHitboxes().Run();
 
 			// DebugOverlay.Box( tr.EndPos, Vector3.One * -1, Vector3.One, Color.Red );
 
@@ -37,7 +37,7 @@ namespace Minigolf
 
 			Viewer = null;
 			{
-				var lerpTarget = tr.EndPos.Distance( Pos );
+				var lerpTarget = tr.EndPos.Distance( Position );
 
 				DoFPoint = lerpTarget;// DoFPoint.LerpTo( lerpTarget, Time.Delta * 10 );
 			}
@@ -73,13 +73,13 @@ namespace Minigolf
 
 		void FreeMove()
 		{
-			var mv = MoveInput.Normal * 300 * RealTime.Delta * Rot * MoveSpeed;
+			var mv = MoveInput.Normal * 300 * RealTime.Delta * Rotation * MoveSpeed;
 
 			TargetRot = Rotation.From( LookAngles );
 			TargetPos += mv;
 
-			Pos = Vector3.Lerp( Pos, TargetPos, 10 * RealTime.Delta * (1 - LerpMode) );
-			Rot = Rotation.Slerp( Rot, TargetRot, 10 * RealTime.Delta * (1 - LerpMode) );
+			Position = Vector3.Lerp( Position, TargetPos, 10 * RealTime.Delta * (1 - LerpMode) );
+			Rotation = Rotation.Slerp( Rotation, TargetRot, 10 * RealTime.Delta * (1 - LerpMode) );
 		}
 	}
 }
