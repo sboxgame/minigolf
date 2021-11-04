9cae69bf010cab1298871b475d26b76b82a7176f
Matt Stevens <matt@mattstevens.co.uk>
2021-11-04 05:22:58 +0000

    Stop freecam when in play or cupped, add UI indicator of free cam, experiment with limiting time to challenge users on tricky hole layouts

--- code/UI/Hud.cs
+++ code/UI/Hud.cs
@@ -8,11 +8,22 @@ namespace Minigolf
 	{
 		public WaitingScreen WaitingScreen { get; set; }
 
+		public Panel Freecam { get; set; }
+		public string FreecamTime => $"00:{ Game.Current.FreeCamTimeLeft.CeilToInt().ToString( "D2" ) }";
+
 		public override void Tick()
 		{
 			base.Tick();
 
 			SetClass( "state--playing", Game.Current.State == GameState.Playing );
+
+			Freecam?.SetClass( "freecam--active", Game.Current.FreeCamera != null );
+		}
+
+		protected override void PostTemplateApplied()
+		{
+			if ( Game.Current.State != GameState.WaitingForPlayers )
+				WaitingScreen?.Delete( false );
 		}
 
 		[Event( "minigolf.state.changed" )]
