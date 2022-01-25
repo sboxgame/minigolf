31e0331a0ddc54b97fdd7b194aae701558a3e4d9
Matt Stevens <matt@mattstevens.co.uk>
2022-01-25 19:36:49 +0000

    Update trap entity code after merge

--- code/Entity/Map/Trap.cs
+++ code/Entity/Map/Trap.cs
@@ -1,68 +1,67 @@
 ﻿using Sandbox;
 using System;
 
-namespace Minigolf
+namespace Facepunch.Minigolf.Entities;
+
+[Library( "minigolf_trap" )]
+[Hammer.Solid]
+[Hammer.AutoApplyMaterial( "materials/editor/minigolf_wall/minigolf_trap.vmat" )]
+[Hammer.VisGroup( Hammer.VisGroup.Trigger )]
+public partial class Trap : ModelEntity
 {
-	[Library( "minigolf_trap" )]
-	[Hammer.Solid]
-	[Hammer.AutoApplyMaterial( "materials/editor/minigolf_wall/minigolf_trap.vmat" )]
-	[Hammer.VisGroup( Hammer.VisGroup.Trigger )]
-	public partial class Trap : ModelEntity
+
+	[Property( "Particle" ), ResourceType( "vpcf" )]
+	public string ParticleEffect { get; set; } = "particles/gameplay/ball_water_splash/ball_water_splash.vpcf";
+
+	/// <summary>
+	/// Name of the sound to play.
+	/// </summary>
+	[Property( "soundName" ), FGDType( "sound" )]
+	public string SoundName { get; set; } = "minigolf.ball_in_water";
+
+	[Property("Particle Tint" )]
+	[Net] public Color ParticleTint { get; set; }= Color.Black;
+
+
+	public override void Spawn()
 	{
+		base.Spawn();
 
-		[Property( "Particle" ), ResourceType( "vpcf" )]
-		public string ParticleEffect { get; set; } = "particles/gameplay/ball_water_splash/ball_water_splash.vpcf";
+		var PhysGroup = SetupPhysicsFromModel( PhysicsMotionType.Static );
+		PhysGroup?.SetSurface( "water" );
 
-		/// <summary>
-		/// Name of the sound to play.
-		/// </summary>
-		[Property( "soundName" ), FGDType( "sound" )]
-		public string SoundName { get; set; } = "minigolf.ball_in_water";
+		ClearCollisionLayers();
+		AddCollisionLayer( CollisionLayer.Water );
+		AddCollisionLayer( CollisionLayer.Trigger );
+		EnableSolidCollisions = false;
+		EnableTouch = true;
+		EnableTouchPersists = false;
 
-		[Property("Particle Tint" )]
-		[Net] public Color ParticleTint { get; set; }= Color.Black;
+		Transmit = TransmitType.Never;
+	}
+
+	public override void StartTouch( Entity other )
+	{
+		if ( other is not Ball ball )
+			return;
+
+		ball.InWater = true;
+
+		Sound.FromWorld( SoundName, ball.Position );
+		var ParticleTrap = Particles.Create( ParticleEffect, ball.Position );
+		ParticleTrap.SetPosition( 2, ParticleTint * 255 );
 
 
-		public override void Spawn()
+		Action task = async () =>
 		{
-			base.Spawn();
+			await Task.DelaySeconds( 2 );
 
-			var PhysGroup = SetupPhysicsFromModel( PhysicsMotionType.Static );
-			PhysGroup?.SetSurface( "water" );
-
-			ClearCollisionLayers();
-			AddCollisionLayer( CollisionLayer.Water );
-			AddCollisionLayer( CollisionLayer.Trigger );
-			EnableSolidCollisions = false;
-			EnableTouch = true;
-			EnableTouchPersists = false;
-
-			Transmit = TransmitType.Never;
-		}
-
-		public override void StartTouch( Entity other )
-		{
-			if ( other is not Ball ball )
+			if ( !other.IsValid() )
 				return;
 
-			ball.InWater = true;
-
-			Sound.FromWorld( SoundName, ball.Position );
-			var ParticleTrap = Particles.Create( ParticleEffect, ball.Position );
-			ParticleTrap.SetPosition( 2, ParticleTint * 255 );
-
-
-			Action task = async () =>
-			{
-				await Task.DelaySeconds( 2 );
-
-				if ( !other.IsValid() )
-					return;
-
-				if ( other is Ball ball )
-						Game.Current.BallOutOfBounds( ball, Game.OutOfBoundsType.Water );
-			};
-			task.Invoke();
-		}
+			if ( other is Ball ball )
+					Game.Current.BallOutOfBounds( ball, Game.OutOfBoundsType.Water );
+		};
+		task.Invoke();
 	}
 }
