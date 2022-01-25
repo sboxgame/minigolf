6faad458f665f79f7dfd74ccd6ce7d9e0e3f20e8
Matt Stevens <matt@mattstevens.co.uk>
2022-01-25 21:16:33 +0000

    Update control hints to use Input.GetGlyph with light neutral style

--- code/UI/ControlsHelp.cs
+++ code/UI/ControlsHelp.cs
@@ -2,57 +2,32 @@
 using Sandbox;
 using Sandbox.UI;
 using Sandbox.UI.Construct;
-using System.Threading.Tasks;
 
 namespace Facepunch.Minigolf.UI;
 
 [Library]
 public class ButtonHint : Panel
 {
-	[Property]
-	public string Name { get; set; }
-
-	public InputButton InputButton { get; set; }
-
-	public Label LabelAction { get; set; }
-	public Image ImageGlyph { get; set; }
+	InputButton InputButton { get; set; }
+	Image ImageGlyph { get; init; }
 
 	public ButtonHint()
 	{
 		ImageGlyph = Add.Image();
-		LabelAction = Add.Label();
 	}
 
-	public override void Tick()
+    public override void SetProperty(string name, string value)
+    {
+        base.SetProperty(name, value);
+
+		if ( name == "name" )
+			InputButton = System.Enum.Parse<InputButton>(value, true);
+	}
+
+    public override void Tick()
 	{
-		InputButton = System.Enum.Parse<InputButton>(Name, true);
-
-		LabelAction.SetText(Name);
-		ImageGlyph.Texture = Input.GetGlyph(InputButton, InputGlyphSize.Small);
-
+		ImageGlyph.Texture = Input.GetGlyph( InputButton, InputGlyphSize.Small, GlyphStyle.Light.WithNeutralColorABXY() );
 		ImageGlyph.Style.Width = ImageGlyph.Texture.Width;
 		ImageGlyph.Style.Height = ImageGlyph.Texture.Height;
-	}
-}
-
-public partial class ControlsHelp : Panel
-{
-	public ControlsHelp()
-	{
-		StyleSheet.Load( "/UI/ControlsHelp.scss" );
-
-		AddButton( "Free Cam", "iv_view" );
-		AddButton( "Reset", "iv_reload" );
-		AddButton( "Stroke", "iv_attack" );
-		AddButton( "Scoreboard", "iv_score" );
-	}
-
-	protected void AddButton( string name, string bind )
-	{
-		var key = Input.GetKeyWithBinding( bind ) ?? "Unbound";
-
-		var row = Add.Panel( "row" );
-		row.Add.Label( name, "name" );
-		row.Add.Label( key, "key" );
 	}
 }
