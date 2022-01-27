6b7a640ff2985dffe13fdd0423c2eca036f4b2f7
bakscratch <lt_8@hotmail.co.uk>
2022-01-27 11:41:15 +0000

    Create particle on flag with hole number

--- code/Entity/Map/HoleGoal.cs
+++ code/Entity/Map/HoleGoal.cs
@@ -18,6 +18,8 @@ public partial class HoleGoal : ModelEntity
 	[Property, Net]
 	public int HoleNumber { get; set; }
 
+	public Particles HoleParticle { get; set; }
+
 	public override void Spawn()
 	{
 		base.Spawn();
@@ -26,8 +28,25 @@ public partial class HoleGoal : ModelEntity
 		CollisionGroup = CollisionGroup.Trigger;
 		EnableSolidCollisions = false;
 		EnableTouch = true;
-		EnableDrawing = false;
-		Transmit = TransmitType.Never;
+		EnableDrawing = true;
+		Transmit = TransmitType.Always;
+
+	}
+
+	public override void ClientSpawn()
+	{
+		base.ClientSpawn();
+
+
+		HoleParticle = Particles.Create( "particles/gameplay/flag_number/flag_number.vpcf", this );
+
+		var number = HoleNumber;
+
+		HoleParticle.SetPositionComponent(21,2,number%10);
+
+		number /= 10;
+		HoleParticle.SetPositionComponent(21,1,number%10);
+
 	}
 
 	public override void StartTouch( Entity other )
