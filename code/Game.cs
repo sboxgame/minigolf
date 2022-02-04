997d7c2b6a9cf26fa069d3d11630f26139e6c61f
Crayz <jacob.ee92@gmail.com>
2022-02-04 15:03:29 -0700

    Reload Holes on hotload to avoid NRE spam caused by BaseNetworkable hotload exception

--- code/Game.cs
+++ code/Game.cs
@@ -23,6 +23,15 @@ public partial class Game : Sandbox.Game
 		}
 	}
 
+	[Event.Hotload]
+	private void OnHotload()
+    {
+		// sometings fucked with BaseNetworkable during hotload, data gets lost
+		// just make a new Course for now to avoid nre spam
+		if ( IsClient ) return;
+		Course.LoadFromMap();
+    }
+
 	public override void ClientJoined( Client cl )
 	{
 		Log.Info( $"\"{cl.Name}\" has joined the game" );
