e86503fa6ef3b219b4c9d3c448975d3ba1181279
Alex Guthrie <12881812+xezno@users.noreply.github.com>
2023-06-22 15:36:57 +0100

    Implement basic menu navigation system

---
+++ code/UI/GameMenu/Menus/BaseMenu.cs
@@ -0,0 +1,19 @@
+﻿namespace Facepunch.Minigolf.UI;
+
+public class BaseMenu : Panel, INavigatorPage
+{
+	public BaseMenu()
+	{
+		AddClass( "menu" );
+	}
+
+	public void GoTo( Type menu )
+	{
+		var name = "/" + menu.Name.ToLower();
+
+		Log.Info( "Navigating to " + name );
+
+		this.Delete();
+		this.Navigate( name );
+	}
+}
