8a39db54efba2083e71d1c4346fee5375ecc7e81
Tony Ferguson <devultj@gmail.com>
2024-10-09 11:16:25 +0100

    Add Hole.Par

--- Code/GameLoop/Hole.cs
+++ Code/GameLoop/Hole.cs
@@ -6,8 +6,11 @@ public sealed class Hole : Component
 	[Property]
 	public int Number { get; set; } = 1;
 
+	[Property]
+	public int Par { get; set; } = 3;
+
 	public override string ToString()
 	{
-		return $"Hole {Number}";
+		return $"Hole {Number}, Par {Par}";
 	}
 }
