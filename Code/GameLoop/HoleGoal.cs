2c6e91a1199d22a46f557fa0bd3b3a7eddb63299
Tony Ferguson <devultj@gmail.com>
2024-10-09 10:19:46 +0100

    Set up hole number particle

--- Code/GameLoop/HoleGoal.cs
+++ Code/GameLoop/HoleGoal.cs
@@ -6,6 +6,9 @@ public sealed class HoleGoal : Component, Component.ITriggerListener
 	[Property] 
 	public Hole Hole { get; set; }
 
+	[Property]
+	public LegacyParticleSystem Particles { get; set; }
+
 	void ITriggerListener.OnTriggerEnter( Collider other )
 	{
 		var ball = other.GameObject.Root.GetComponentInChildren<Ball>();
@@ -13,5 +16,12 @@ public sealed class HoleGoal : Component, Component.ITriggerListener
 			return;
 
 		IGameEvent.Post( x => x.OnGoal( ball, this ) );
+	}
+
+	protected override void OnStart()
+	{
+		var firstNum = Hole.Number % 10;
+		var secondNum = ( Hole.Number / 10 ) % 10;
+		Particles.SceneObject.SetControlPoint( 21, new Vector3( 0, firstNum, secondNum ) );
 	}
 }
