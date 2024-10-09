dfdba8c2880552a5a5dbd64324d419f4c15b4f65
Tony Ferguson <devultj@gmail.com>
2024-10-09 14:28:05 +0100

    Restore HoleMarker

--- Code/GameLoop/Hole.cs
+++ Code/GameLoop/Hole.cs
@@ -6,8 +6,16 @@ public sealed class Hole : Component
 	[Property]
 	public int Number { get; set; } = 1;
 
+	/// <summary>
+	/// The par
+	/// </summary>
 	[Property]
 	public int Par { get; set; } = 3;
+
+	/// <summary>
+	/// The goal
+	/// </summary>
+	public HoleGoal Goal => Scene.GetAllComponents<HoleGoal>().FirstOrDefault( x => x.Hole == this );
 
 	public override string ToString()
 	{
